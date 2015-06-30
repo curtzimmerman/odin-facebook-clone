ce699e5c21a4b07433bd95da55a1eee2594aaf90
Curt Zimmerman <czimmerman9@gmail.com>
2015-06-30 13:27:49 -0400

    add additional parameters to devise registration

--- db/schema.rb
+++ db/schema.rb
@@ -19,10 +19,8 @@ ActiveRecord::Schema.define(version: 20150630054759) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email",                  default: "", null: false
-    t.string   "password_digest"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "remember_digest"
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
